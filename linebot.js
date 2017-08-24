913d12aaee3440729fbe3f6b864363660ea1efc8
jackymanbo <jackymanbo@gmail.com>
2017-08-24 23:59:17 +0800

    asd

--- linebot.js
+++ linebot.js
@@ -2,9 +2,9 @@ var linebot = require('linebot');
 var express = require('express');
 
 var bot = linebot({
-  channelId: 'f0759fd2cfdc572eb73688a6cb311b24',
-  channelSecret: 'FNuFKN3CCpcWPP9R+9Kl/Bw+Csg5jv1wval3vYt7Mr8Dhlx2nQ/pkiYj0iwQd+O9ucybITPTOAk2zkJ1bgUb18FEeF8oCn+rFdlJ4fsFdE32FQ25TGV25ii00mJ+goR4XMlf+cA5b691L45jL3eS9gdB04t89/1O/w1cDnyilFU=',
-  channelAccessToken: 'U516b551c6400dd7fa01f1a203a4b6865'
+  channelId: '1531798185',
+  channelSecret: 'f0759fd2cfdc572eb73688a6cb311b24',
+  channelAccessToken: 'FNuFKN3CCpcWPP9R+9Kl/Bw+Csg5jv1wval3vYt7Mr8Dhlx2nQ/pkiYj0iwQd+O9ucybITPTOAk2zkJ1bgUb18FEeF8oCn+rFdlJ4fsFdE32FQ25TGV25ii00mJ+goR4XMlf+cA5b691L45jL3eS9gdB04t89/1O/w1cDnyilFU='
 });
 
 bot.on('message', function (event) {
